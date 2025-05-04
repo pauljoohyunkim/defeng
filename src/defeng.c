cbb0f8a1dbe0a1a7d831b3ac3d760e46facae113
pauljoohyunkim <pauljoohyunkim@gmail.com>
2025-05-04 10:03:58 +0900

    Option parsing started

--- src/defeng.c
+++ src/defeng.c
@@ -1,6 +1,22 @@
+#include <getopt.h>
 #include <stdio.h>
 
-int main()
+int main(int argc, char** argv)
 {
-    
+    int opt;
+
+    // Parsing arguments
+    while((opt = getopt(argc, argv, ":hc:v:o:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'h':
+            case 'c':
+            case 'v':
+            case 'o':
+            case '?':
+                printf("Unknown option: %c\n", optopt);
+                break;
+        }
+    }
 }
