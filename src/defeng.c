e90885e1279806bdaa574834da10c2405fdaed89
pauljoohyunkim <pauljoohyunkim@gmail.com>
2025-05-04 10:38:34 +0900

    Filename

--- src/defeng.c
+++ src/defeng.c
@@ -4,6 +4,9 @@
 int main(int argc, char** argv)
 {
     int opt;
+    char* consonant_filename = NULL;
+    char* vowel_filename = NULL;
+    char* output_filename = NULL;
 
     // Parsing arguments
     while((opt = getopt(argc, argv, ":hc:v:o:")) != -1)
@@ -12,11 +15,22 @@ int main(int argc, char** argv)
         {
             case 'h':
             case 'c':
+                consonant_filename = optarg;
+                break;
             case 'v':
+                vowel_filename = optarg;
+                break;
             case 'o':
+                output_filename = optarg;
+                break;
+            case ':':
+                printf("Argument needed for %c\n", optopt);
+                break;
             case '?':
                 printf("Unknown option: %c\n", optopt);
                 break;
         }
     }
+
+
 }
